1dfbc589a000288b68198fcf823c1eddf1d66814
Andrew Obiano <andrew.obiano@gmail.com>
2020-02-22 22:19:53 -1000

    adds link icons and scss

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import styles from "./About.module.scss";
+import head from "../../assets/jonhead-revA-square.png";
 
 const About = () => {
   return (
-    <section className={styles.About}>About me description goes here</section>
+    <section className={styles.about}>
+      <h2>Who I Am</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        maxime illo eum dolorem voluptatem eveniet quos, fuga veniam eos!
+        Delectus, nisi odit animi perferendis alias maiores voluptatem culpa
+        fugiat quam! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Maiores iste voluptatibus vel voluptates at unde modi asperiores
+        obcaecati. Laborum dicta porro accusamus maxime consequuntur ea voluptas
+        numquam harum excepturi suscipit.
+      </p>
+      <span>
+        <img className={styles.pic} src={head} alt="headshot" />
+      </span>
+    </section>
   );
 };
 
