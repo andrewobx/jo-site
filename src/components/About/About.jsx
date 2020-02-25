4227d76e3d40c3ebf2a543e247f6e7e5c8f0a62e
Andrew Obiano <andrew.obiano@gmail.com>
2020-02-24 23:45:30 -1000

    update styles and div organization

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,19 +5,20 @@ import head from "../../assets/jonhead-revA-square.png";
 const About = () => {
   return (
     <section className={styles.about}>
-      <h2>Who I Am</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-        maxime illo eum dolorem voluptatem eveniet quos, fuga veniam eos!
-        Delectus, nisi odit animi perferendis alias maiores voluptatem culpa
-        fugiat quam! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-        Maiores iste voluptatibus vel voluptates at unde modi asperiores
-        obcaecati. Laborum dicta porro accusamus maxime consequuntur ea voluptas
-        numquam harum excepturi suscipit.
-      </p>
-      <span>
-        <img className={styles.pic} src={head} alt="headshot" />
-      </span>
+      <div className={styles.text}>
+        <h2>Who I Am</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+          maxime illo eum dolorem voluptatem eveniet quos, fuga veniam eos!
+          Delectus, nisi odit animi perferendis alias maiores voluptatem culpa
+          fugiat quam! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Maiores iste voluptatibus vel voluptates at unde modi asperiores
+          obcaecati. Laborum dicta porro accusamus maxime consequuntur ea
+          voluptas numquam harum excepturi suscipit.
+        </p>
+      </div>
+
+      <img className={styles.pic} src={head} alt="headshot" />
     </section>
   );
 };
--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./MainPage.module.scss";
 import mainPic from "../../assets/jongraphFINAL.png";
+import secondaryPic from "../../assets/jonPano.jpg";
 import Links from "../Links";
 import About from "../About";
 
@@ -8,11 +9,24 @@ const MainPage = () => {
   return (
     <div className={styles.main}>
       <section className={styles.hero}>
-        <img className={styles.pic} src={mainPic} alt="main background" />
+        <img className={styles.mainPic} src={mainPic} alt="main background" />
         <h1 className={styles.name}>JONATHAN OBIANO</h1>
+        <div>
+          <img
+            className={styles.secondPic}
+            src={secondaryPic}
+            alt="secondary background"
+          />
+          <div className={styles.musicLink}>
+            <h2>Check out my new music</h2>
+            <a href="/">
+              <h1>LISTEN</h1>
+            </a>
+          </div>
+        </div>
+        <Links />
+        <About />
       </section>
-      <Links />
-      <About />
     </div>
   );
 };
