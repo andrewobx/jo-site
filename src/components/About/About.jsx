66a0836bda327eec1a97acdf74e660cdf6f0b145
Andrew Obiano <andrew.obiano@gmail.com>
2020-03-03 00:16:08 -1000

    adds aos to about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import styles from "./About.module.scss";
 import head from "../../assets/jonhead-revA-square.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const About = () => {
+  AOS.init();
+
   return (
     <section className={styles.about}>
       <div className={styles.text}>
-        <h2>Who is Jonathan?</h2>
-        <p>
+        <h2 data-aos="fade-up">Who is Jonathan?</h2>
+        <p data-aos="fade-up">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
           maxime illo eum dolorem voluptatem eveniet quos, fuga veniam eos!
           Delectus, nisi odit animi perferendis alias maiores voluptatem culpa
@@ -18,7 +22,9 @@ const About = () => {
         </p>
       </div>
 
-      <img className={styles.pic} src={head} alt="headshot" />
+      <div className={styles.pic}>
+        <img data-aos="fade-up" src={head} alt="headshot" />
+      </div>
     </section>
   );
 };
